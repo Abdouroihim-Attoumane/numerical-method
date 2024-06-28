78fc53af933798de2b04ee394f8926be486c1b73
Abdhm23 <abdoumars23@gmail.com>
2024-06-28 17:24:21 +0000

    amelioration du modul matrice

--- AbdhmMN/AbdhmMN/matrice.py
+++ AbdhmMN/AbdhmMN/matrice.py
@@ -2,10 +2,43 @@
 
 import numpy as np
 
+#--------------------------------------------------------------
+
+#La fonction Tri-diagonal(n) construit la matrice diagonale demandée, de taille nxn
+
+def TriD(taille, diag_valeur, sousdiag_valeur, surdiag_valeur):
+
+    A=np.zeros((taille,taille))
+    A[taille-1,taille-1] =  diag_valeur
+
+    for i in range(n-1):
+        A[i,i] = diag_valeur
+        A[i+1,i] = sousdiag_valeur
+        A[i,i+1] = surdiag_valeur
+
+    return A
+
+#--------------------------------------------------------------
+
+#Permutation des colonnes de la matrice M les une à près les autres 
+
+def PermutCol(M):
+
+    (m,n)=np.shape(M);
+    #on détermine le nombre de lignes et de colonnes de M
+    N=np.zeros((m,n));#on crée une matrice de même taille
+    #On remplit ensuite les colonnes de cette nouvelle matrice:
+    N[0:m,0:n-1]=M[0:m,1:n]
+    N[0:m,n-1]=M[0:m,0]
+    return N
+
+#--------------------------------------------------------------
+
 # La matrice de Vandermonde sans boucle
-#x doit être de taille (n,0): x=[a,b,c, ...] dans les quatre fonctions ci-dessous.
+#le vecteur doit être de taille (n,0): x=[a,b,c, ...] dans les quatre fonctions ci-dessous.
 
 def vand(x):
+
     n=np.size(x) #taille de la matrice A
     #on convertit x en une matrice X de taille (1,n)
     X=np.zeros((1,n))
